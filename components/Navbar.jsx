b206f64725c975b94f5e21bb7869821176457ba9
Asilbec <asilbekomonkulov2003@gmail.com>
2022-06-13 08:28:44 -0400

    fixed

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import Link from 'next/link';
 import { AiOutlineShopping } from 'react-icons/ai'
-
+import piclog from  '../public/logos.jpg'
 import { Cart } from './';
 import { useStateContext} from '../context/StateContext';
+import Image from 'next/image'
 
 const Navbar = () => {
   const { showCart, setShowCart, totalQuantities } = useStateContext();
 
   return (
     <div className="navbar-container">
-      <p className="logo">
-        <Link href="/">Bekware</Link>
-      </p>
+      <div className="logo">
+        <Link href="/">
+        <Image src={piclog} className='navbar-logo' alt="me" width="35" height="35" />
+        </Link>
+      </div>
 
       <button type="button" className="cart-icon" onClick={() => setShowCart(true)}>
         <AiOutlineShopping />
